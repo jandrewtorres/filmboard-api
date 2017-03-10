dc43067cca50f31dbdd585b2b79f5246efb22ccd
Andrew Torres <jandrewtorres@gmail.com>
2017-03-10 14:14:28 -0800

    diff URL

--- src/routes.php
+++ src/routes.php
@@ -9,7 +9,7 @@ $app->options('/{routes:.+}', function ($request, $response, $args) {
 $app->add(function ($req, $res, $next) {
     $response = $next($req, $res);
     return $response
-            ->withHeader('Access-Control-Allow-Origin', 'localhost:3000')
+            ->withHeader('Access-Control-Allow-Origin', 'http://localhost:3000')
             ->withHeader('Access-Control-Allow-Headers', 'X-Requested-With, Content-Type, Accept, Origin, Authorization')
             ->withHeader('Access-Control-Allow-Methods', 'GET, POST, PUT, DELETE, OPTIONS');
 });
