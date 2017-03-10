0f29d305f29e46c0cb4a5170d913cca0cf275c6f
Andrew Torres <jandrewtorres@gmail.com>
2017-03-10 14:58:24 -0800

    CORS

--- src/routes.php
+++ src/routes.php
@@ -9,7 +9,7 @@ $app->add(function ($req, $res, $next) {
     $response = $next($req, $res);
     $route = $req->getAttribute('route');
     return $response
-            ->withHeader('Access-Control-Allow-Origin', '${route}')
+            ->withHeader('Access-Control-Allow-Origin', 'http://localhost:3000')
             ->withHeader('Access-Control-Allow-Headers', 'X-Requested-With, Content-Type, Accept, Origin, Authorization')
             ->withHeader('Access-Control-Allow-Methods', 'GET, POST, PUT, DELETE, OPTIONS');
 });
