5b8d9c64ff858607cd3a4de4c3bcd381fd8bc0d9
Andrew Torres <jandrewtorres@gmail.com>
2017-03-10 14:54:13 -0800

    variable route CORS

--- src/routes.php
+++ src/routes.php
@@ -1,6 +1,20 @@
 <?php
+//CORS
 
-// Routes
+$app->options('/{routes:.+}', function ($request, $response, $args) {
+    return $response;
+});
+
+$app->add(function ($req, $res, $next) {
+    $response = $next($req, $res);
+    $route = $req->getAttribute('route');
+    return $response
+            ->withHeader('Access-Control-Allow-Origin', $route)
+            ->withHeader('Access-Control-Allow-Headers', 'X-Requested-With, Content-Type, Accept, Origin, Authorization')
+            ->withHeader('Access-Control-Allow-Methods', 'GET, POST, PUT, DELETE, OPTIONS');
+});
+
+// ROUTES
 
 // Home Message
 $app->get('/', function ($request, $response, $args) {
