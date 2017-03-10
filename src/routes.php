0263d61cc78dd6713caa3a8fe23c895337bbe9d0
Andrew Torres <jandrewtorres@gmail.com>
2017-03-10 15:02:41 -0800

    omg *

--- src/routes.php
+++ src/routes.php
@@ -7,9 +7,8 @@ $app->options('/{routes:.+}', function ($request, $response, $args) {
 
 $app->add(function ($req, $res, $next) {
     $response = $next($req, $res);
-    $route = $req->getAttribute('route');
     return $response
-            ->withHeader('Access-Control-Allow-Origin', 'http://localhost:3000')
+            ->withHeader('Access-Control-Allow-Origin', '*')
             ->withHeader('Access-Control-Allow-Headers', 'X-Requested-With, Content-Type, Accept, Origin, Authorization')
             ->withHeader('Access-Control-Allow-Methods', 'GET, POST, PUT, DELETE, OPTIONS');
 });
