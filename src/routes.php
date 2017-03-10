8cb98535a82a6c65356597c841a5e8d7d4f482ea
Andrew Torres <jandrewtorres@gmail.com>
2017-03-10 14:09:18 -0800

    added CORS

--- src/routes.php
+++ src/routes.php
@@ -1,6 +1,19 @@
 <?php
 // Routes
 
+// CORS Lazy
+$app->options('/{routes:.+}', function ($request, $response, $args) {
+    return $response;
+});
+
+$app->add(function ($req, $res, $next) {
+    $response = $next($req, $res);
+    return $response
+            ->withHeader('Access-Control-Allow-Origin', 'http://mysite')
+            ->withHeader('Access-Control-Allow-Headers', 'X-Requested-With, Content-Type, Accept, Origin, Authorization')
+            ->withHeader('Access-Control-Allow-Methods', 'GET, POST, PUT, DELETE, OPTIONS');
+});
+
 // Home Message
 $app->get('/', function ($request, $response, $args) {
    echo "FilmBoard API";
