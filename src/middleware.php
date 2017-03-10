5067ec9e2247747b6f7df6090c135981d6f7cfe3
Andrew Torres <jandrewtorres@gmail.com>
2017-03-10 15:13:21 -0800

    slimCors middleware

--- src/middleware.php
+++ src/middleware.php
@@ -2,3 +2,9 @@
 // Application middleware
 
 // e.g: $app->add(new \Slim\Csrf\Guard);
+$corsOptions = array(
+    "origin" => "*",
+    "exposeHeaders" => array("Content-Type", "X-Requested-With", "X-authentication", "X-client"),
+    "allowMethods" => array('GET', 'POST', 'PUT', 'DELETE', 'OPTIONS')
+);
+$app->add(new /CorsSlim/CorsSlim($corsOptions));
