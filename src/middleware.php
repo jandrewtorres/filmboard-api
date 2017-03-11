b9f802e2126ca76719212a07734a8c1344d81c4a
Andrew Torres <jandrewtorres@gmail.com>
2017-03-10 23:23:42 -0800

    lazy CORS

--- src/middleware.php
+++ src/middleware.php
@@ -2,3 +2,15 @@
 // Application middleware
 
 // e.g: $app->add(new \Slim\Csrf\Guard);
+
+$app->options('/{routes:.+}', function ($request, $response, $args) {
+    return $response;
+});
+
+$app->add(function ($req, $res, $next) {
+    $response = $next($req, $res);
+    return $response
+            ->withHeader('Access-Control-Allow-Origin', 'https://filmboard-api.herokuapp.com/')
+            ->withHeader('Access-Control-Allow-Headers', 'X-Requested-With, Content-Type, Accept, Origin, Authorization')
+            ->withHeader('Access-Control-Allow-Methods', 'GET, POST, PUT, DELETE, OPTIONS');
+});
