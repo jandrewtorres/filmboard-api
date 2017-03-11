7b290207d1876de617f9f2cd91cefa7d8ab01897
Andrew Torres <jandrewtorres@gmail.com>
2017-03-11 00:24:09 -0800

    try

--- src/routes.php
+++ src/routes.php
@@ -3,7 +3,7 @@
 
 // Home Message
 $app->get('/', function ($request, $response, $args) {
-   $sql = "SELECT TABLE_NAME as table, TABLES.* FROM INFORMATION_SCHEMA.TABLES WHERE TABLE_SCHEMA='database_name' GROUP BY table;
+   $sql = "SELECT TABLE_NAME as table, TABLES.* FROM INFORMATION_SCHEMA.TABLES GROUP BY table;
 ";
    $stmt = $this->db->prepare($sql);
    $stmt->execute();
