f2db69226252c1375f994be3e6e7907b2a3944b3
Andrew Torres <jandrewtorres@gmail.com>
2017-03-11 16:59:53 -0800

    preflight

--- src/middleware.php
+++ src/middleware.php
@@ -3,18 +3,26 @@
 
 // e.g: $app->add(new \Slim\Csrf\Guard);
 
-$app->options('/{category}/{name}', function ($request, $response, $args) {
-	$response = $next($request, $response);
-    return $response
-        ->withHeader('Access-Control-Allow-Origin', '*')
-        ->withHeader('Access-Control-Allow-Headers', 'X-Requested-With, Content-Type, Accept, Origin, Authorization')
-        ->withHeader('Access-Control-Allow-Methods', 'GET, POST, PUT, DELETE, OPTIONS');
-});
+$app->add(function($request, $response, $next) {
+    $route = $request->getAttribute("route");
 
-$app->add(function ($req, $res, $next) {
-    $response = $next($req, $res);
-    return $response
-            ->withHeader('Access-Control-Allow-Origin', '*')
-            ->withHeader('Access-Control-Allow-Headers', 'X-Requested-With, Content-Type, Accept, Origin, Authorization')
-            ->withHeader('Access-Control-Allow-Methods', 'GET, POST, PUT, DELETE, OPTIONS');
+    $methods = [];
+
+    if (!empty($route)) {
+        $pattern = $route->getPattern();
+
+        foreach ($this->router->getRoutes() as $route) {
+            if ($pattern === $route->getPattern()) {
+                $methods = array_merge_recursive($methods, $route->getMethods());
+            }
+        }
+        //Methods holds all of the HTTP Verbs that a particular route handles.
+    } else {
+        $methods[] = $request->getMethod();
+    }
+    
+    $response = $next($request, $response);
+
+
+    return $response->withHeader("Access-Control-Allow-Methods", implode(",", $methods));
 });
